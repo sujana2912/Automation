1dd513283f3fdd3b9fa6522afcd685204c0d8669
sujanaarava2010@gmail.com <sujanaarava2010@gmail.com>
2025-10-06 15:07:51 +0530

    Commit

--- src/main/java/homePages/LoginPage.java
+++ src/main/java/homePages/LoginPage.java
@@ -8,10 +8,10 @@ import base.TestBase;
 
 public class LoginPage extends TestBase {
 
-    @FindBy(xpath="//input[@placeholder='Username']")
+    @FindBy(xpath="//input[@placeholder='Username1']")
     WebElement username;
 
-    @FindBy(xpath="//input[@placeholder='Password']")
+    @FindBy(xpath="//input[@placeholder='Password1']")
     WebElement password;
 
     @FindBy(xpath ="//button[@class='login100-form-btn']")
